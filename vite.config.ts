1c95902cfa5aba54b22664ea32dbdb8706e62132
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2025-05-30 11:05:34 +0530

    chore(workflow): update pipeline for build in github actions 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [tailwindcss()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        rollupOptions: {
+            external: [],
         },
     },
 });
